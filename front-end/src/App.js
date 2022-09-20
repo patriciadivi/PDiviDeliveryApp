9c5aa99e4ba696c72b142c29da249388486d9dda
Debora Schuch <starambients@gmail.com>
2022-09-20 21:02:44 +0200

    chore: defining the right branch

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,7 +5,7 @@ import rockGlass from './images/rockGlass.svg';
 function App() {
   return (
     <div className="App">
-      <span className="logo">TRYBE - Grupo 16</span>
+      <span className="logo">TRYBE - Grupo 16 - acertando a branch2</span>
       <object className="rocksGlass" type="image/svg+xml" data={rockGlass}>
         Glass
       </object>
