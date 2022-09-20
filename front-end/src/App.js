fc9f5ab313fe04b51ed59f9fb1606a7444931fcf
Debora Schuch <starambients@gmail.com>
2022-09-20 20:15:05 +0200

    chore: iniciando projeto na branch maain-group-16

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,8 +5,8 @@ import rockGlass from './images/rockGlass.svg';
 function App() {
   return (
     <div className="App">
-      <span className="logo">TRYBE</span>
-      <object className="rocksGlass" type="image/svg+xml" data={ rockGlass }>
+      <span className="logo">TRYBE - Grupo 16</span>
+      <object className="rocksGlass" type="image/svg+xml" data={rockGlass}>
         Glass
       </object>
     </div>
