e12512a23c742bae908c02653cdac7bebe9d0bb0
PatriciaDivi <patriciavianad@gmail.com>
2022-09-23 00:27:32 -0300

    feat: :sparkles: Configuring routes on the frontend

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,16 +1,29 @@
 import React from 'react';
-import './App.css';
-import rockGlass from './images/rockGlass.svg';
+import { BrowserRouter } from 'react-router-dom';
+import IndexRouter from './routes';
 
 function App() {
   return (
-    <div className="App">
-      <span className="logo">TRYBE - Grupo 16 - really final</span>
-      <object className="rocksGlass" type="image/svg+xml" data={ rockGlass }>
-        Glass
-      </object>
-    </div>
+    <BrowserRouter>
+      <IndexRouter />
+    </BrowserRouter>
   );
 }
-
 export default App;
+
+// import React from 'react';
+// import './App.css';
+// import rockGlass from './images/rockGlass.svg';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <span className="logo">TRYBE - Grupo 16 - really final</span>
+//       <object className="rocksGlass" type="image/svg+xml" data={ rockGlass }>
+//         Glass
+//       </object>
+//     </div>
+//   );
+// }
+
+// export default App;
