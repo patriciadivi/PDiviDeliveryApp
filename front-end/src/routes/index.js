eee1e0dbbafd5611229ea92d6b1ae62e2772d8f3
PatriciaDivi <patriciavianad@gmail.com>
2022-09-23 00:28:20 -0300

    feat: :sparkles: Iniciando as rotas no front-end

---
+++ front-end/src/routes/index.js
@@ -0,0 +1,36 @@
+import { Route, Routes, Navigate } from 'react-router-dom';
+import React from 'react';
+import Login from '../pages/login';
+// import Catalogo from "../pages/catalago";
+// import CatalogoId from "../components/catalogo.id";
+// import NotFound from "../pages/notfound";
+// import Store from "../pages/store";
+// import Perfil from "../pages/perfil";
+// import Loja from "../pages/loja";
+// import Clube from "../pages/clube";
+// import AppWine from "../pages/appWine";
+// import Eventos from "../pages/eventos";
+// import Ofertas from "../pages/ofertas";
+// import PlanosId from "../pages/planos.id";
+
+function IndexRouter() {
+  return (
+    <Routes>
+      <Route path="/" element={ <Navigate to="/login" replace /> } />
+      <Route path="/login" element={ <Login /> } />
+      {/* <Route path="/produtos" element={<Catalogo />} />
+      <Route path="/catalogo/:id" element={<CatalogoId />} />
+      <Route path="/store" element={<Store />} />
+      <Route path="/perfil" element={<Perfil />} />
+      <Route path="/clube" element={<Clube />} />
+      <Route path="/loja" element={<Loja />} />
+      <Route path="/app" element={<AppWine />} />
+      <Route path="/eventos" element={<Eventos />} />
+      <Route path="/ofertas" element={<Ofertas />} />
+      <Route path="/planos/:id" element={<PlanosId />} />
+      <Route path="*" element={<NotFound />} /> */}
+    </Routes>
+  );
+}
+
+export default IndexRouter;
