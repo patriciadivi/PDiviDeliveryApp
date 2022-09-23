685a4bde750a916fbaa64499200c490d4c4a2662
PatriciaDivi <patriciavianad@gmail.com>
2022-09-23 03:57:13 -0300

    refactor: :recycle: Improving error validations

--- back-end/src/middlewares/loginValidator.js
+++ back-end/src/middlewares/loginValidator.js
@@ -1,20 +1,17 @@
 const validator = require('email-validator');
+const CustomErro = require('../err/CustomErro');
 
 const loginValidator = (req, _res, next) => {
   const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw new CustomErro(400, 'Some data was not reported, please check and try again');
+  }
   const emailIsValid = validator.validate(email);
 
-  if (!emailIsValid) {
-    const erro = new Error('Email validation failed');
-    erro.status = 404;
-    throw erro;
-  }
+  if (!emailIsValid) throw new CustomErro(404, 'Email validation failed');
 
-  if (password.length < 6) {
-    const erro = new Error('Password must be at least 6 characters long');
-    erro.status = 404;
-    throw erro;
-  }
+  if (password.length < 6) throw new CustomErro(404, 'Password must be at least 6 characters long');
 
   next();
 };
