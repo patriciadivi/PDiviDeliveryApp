a368b1ce5b6e311cbd49bc9eb874d60cfb4fec2e
ottomicheletti <michelettiotto@gmail.com>
2022-09-26 16:24:58 -0300

    fix: :bug: Implements a function to read the public secret for JWT from a file in the project root

--- back-end/src/helpers/generate.token.js
+++ back-end/src/helpers/generate.token.js
@@ -1,11 +1,12 @@
 const jwt = require('jsonwebtoken');
+const readSecret = require('./read.secret');
 
 require('dotenv').config();
 
 const generateToken = (data) => {
  const { id, name, email, role } = data;
  const newUser = { id, name, email, role };
- const token = jwt.sign({ data: newUser }, process.env.JWT_SECRET, {
+ const token = jwt.sign({ data: newUser }, readSecret(), {
   expiresIn: '365d',
   algorithm: 'HS256',
  });
--- back-end/src/helpers/validate.token.js
+++ back-end/src/helpers/validate.token.js
@@ -1,9 +1,10 @@
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
 const CustomErro = require('../err/CustomErro');
+const readSecret = require('./read.secret');
 
 const validateToken = (token) => new Promise((resolve, _reject) => {
-    const verifyToken = jwt.verify(token, process.env.JWT_SECRET, (error, decoded) => {
+    const verifyToken = jwt.verify(token, readSecret(), (error, decoded) => {
         if (error) throw new CustomErro(401, 'Expired or invalid token');
         resolve(decoded);
     });
