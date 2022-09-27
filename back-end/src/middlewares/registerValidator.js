e85685fe5ff59fb40f8e3f0c3397b7a567721751
Iara Pedrosa <iaraferreirahg@gmail.com>
2022-09-27 11:17:28 -0300

    fix: :bug: fix registerValidator

--- back-end/src/middlewares/registerValidator.js
+++ back-end/src/middlewares/registerValidator.js
@@ -1,20 +1,24 @@
 const validator = require('email-validator');
 const CustomErro = require('../err/CustomErro');
 
+const charactersValidator = (name, password) => {
+  if (name.length < 12) throw new CustomErro(404, 'Name must be at least 12 characters long');
+
+  if (password.length < 6) throw new CustomErro(404, 'Password must be at least 6 characters long');
+}
+
 const registerValidator = (req, _res, next) => {
   const { name, email, password } = req.body;
-
-  if (name.length < 12) throw new CustomErro(404, 'Name must be at least 12 characters long');
 
   if (!name || !email || !password) {
     throw new CustomErro(400, 'Some data was not reported, please check and try again');
   }
 
+  charactersValidator(name, password)
+
   const emailIsValid = validator.validate(email);
 
   if (!emailIsValid) throw new CustomErro(404, 'Email validation failed');
-
-  if (password.length < 6) throw new CustomErro(404, 'Password must be at least 6 characters long');
 
   next();
 };
