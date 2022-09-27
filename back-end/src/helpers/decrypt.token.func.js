6cea925ba31d8d44d4228915fc8c22959c5cd6ef
PatriciaDivi <patriciavianad@gmail.com>
2022-09-27 17:01:46 -0300

    fix(bugs): :bug: Fix for 'CustomError' imports

--- back-end/src/helpers/decrypt.token.func.js
+++ back-end/src/helpers/decrypt.token.func.js
@@ -1,19 +1,17 @@
-const jwt = require('jsonwebtoken');
-// require('dotenv').config();
-import CustomErro from '../err/CustomErro';
-const jwtKey = require('fs')
-.readFileSync('./jwt.evaluation.key', { encoding: 'utf-8' });
+// const jwt = require('jsonwebtoken');
+//  require('dotenv').config();
+// import CustomError from '../err/CustomError';
+// const jwtKey = require('fs')
+// .readFileSync('./jwt.evaluation.key', { encoding: 'utf-8' });
+// const decryptTokenfunc = (paramsToken) => {
+//   try {
+//     const token = jwt.verify(paramsToken, jwtKey);
+//     const { data } = token;
+//     if (!token) throw new CustomError(401, 'invalid token');
+//     return token;
+//   } catch (error) {
+//     console.error(error.message);
+//   }
+// };
 
-
-const decryptTokenfunc = (paramsToken) => {
-  try {
-    const token = jwt.verify(paramsToken, jwtKey);
-    // const { data } = token;
-    if (!token) throw new CustomErro(401, 'invalid token');
-    return token;
-  } catch (error) {
-    console.error(error.message);
-  }
-};
-
-export default decryptTokenfunc;
+// export default decryptTokenfunc;
--- back-end/src/middlewares/loginValidator.js
+++ back-end/src/middlewares/loginValidator.js
@@ -1,17 +1,19 @@
 const validator = require('email-validator');
-const CustomErro = require('../err/CustomErro');
+const CustomError = require('../err/CustomError');
 
 const loginValidator = (req, _res, next) => {
   const { email, password } = req.body;
 
   if (!email || !password) {
-    throw new CustomErro(400, 'Some data was not reported, please check and try again');
+    throw new CustomError(400, 'Some data was not reported, please check and try again');
   }
   const emailIsValid = validator.validate(email);
 
-  if (!emailIsValid) throw new CustomErro(404, 'Email validation failed');
+  if (!emailIsValid) throw new CustomError(404, 'Email validation failed');
 
-  if (password.length < 6) throw new CustomErro(404, 'Password must be at least 6 characters long');
+  if (password.length < 6) {
+    throw new CustomError(404, 'Password must be at least 6 characters long');
+  }
 
   next();
 };
