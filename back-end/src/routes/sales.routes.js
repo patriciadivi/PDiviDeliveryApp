c7ed63a466f9cc06e3a06aed491c1511998cbdec
Debora Schuch <starambients@gmail.com>
2022-09-28 20:48:15 +0200

    refactor: :recycle: Changing branch as the previous one was already merged

--- back-end/src/routes/sales.routes.js
+++ back-end/src/routes/sales.routes.js
@@ -7,5 +7,6 @@ const router = express.Router({ mergeParams: true });
 // Accessing the route via localhost:3001/sale/
 router.get('/seller', tokenValidator, (req, res) => salesController.getSalesBySellerId(req, res));
 router.get('/:id', tokenValidator, (req, res) => salesController.getSaleById(req, res));
+// DSR tirei a chamada dos pedidos daqui (orders)
 
 module.exports = router;
