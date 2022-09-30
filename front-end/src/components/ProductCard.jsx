7fa9eb5d4e9d2ed209bea1b2f5aeb1770bbff411
ottomicheletti <michelettiotto@gmail.com>
2022-09-30 20:11:42 -0300

    fix: :bug: Fixes req15 where the code were breaking due to a wrong rule

--- front-end/src/components/ProductCard.jsx
+++ front-end/src/components/ProductCard.jsx
@@ -18,7 +18,7 @@ function ProductCard({ product }) {
   const itemQuantity = { ...item, quantity };
 
   useEffect(() => {
-    if (cartItemIndex === menosUm && quantity === 1) {
+    if (cartItemIndex === menosUm && quantity > 0) {
       return insertOnCart(itemQuantity);
     }
     return updateCart(id, itemQuantity);
@@ -47,7 +47,7 @@ function ProductCard({ product }) {
         type="number"
         name="quantity"
         value={ itemQuantity.quantity || 0 }
-        onChange={ ({ target: { value } }) => (Number(value) < 0
+        onChange={ ({ target: { value } }) => (Number(value) <= 0
           ? setQuantity(0)
           : setQuantity(Number(value))) }
       />
