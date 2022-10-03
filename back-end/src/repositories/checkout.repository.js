c0fdc002ad22944b81645baa7118397130959149
ottomicheletti <michelettiotto@gmail.com>
2022-10-03 17:34:06 -0300

    refactor: :recycle: Rewrites the models names for accessing the tables through sequelize

--- back-end/src/repositories/checkout.repository.js
+++ back-end/src/repositories/checkout.repository.js
@@ -1,28 +1,49 @@
-const { sales, users } = require('../database/models');
+const { Sales, Users, SalesProducts, sequelize } = require('../database/models');
 
 const findAll = async (role) => {
-  const usersObj = await users.findAll({ where: { role }, exclude: 'password' });
+  const usersObj = await Users.findAll({ where: { role }, exclude: 'password' });
   return usersObj;
 };
 
-const createOrder = async (
-  { userId, sellerId, totalPrice, deliveryAddress, deliveryNumber }) => {
-  const saleObj = sales.create({
-    userId,
-    sellerId,
-    totalPrice,
-    deliveryAddress,
-    deliveryNumber,
+const createOrder = async (sale) => {
+  const { userId, sellerId, totalPrice, deliveryAddress, deliveryNumber, cart } = sale;
+  // const cartPromises = cart.map((item) => (
+    //   SalesProducts.create({
+    //     saleId: saleObj.dataValues.id,
+    //     productId: item.id,
+    //     quantity: item.quantity,
+    //   })));
+
+    // await Promise.all(cartPromises);
+  const resultCreateSale = sequelize.transaction(async (trans) => {
+    const saleObj = await Sales.create({
+      userId,
+      sellerId,
+      totalPrice,
+      deliveryAddress,
+      deliveryNumber,
+    }, { transaction: trans });
+
+    const idSale = saleObj.dataValues.id;
+
+    const editedCart = cart.map((el) => ({
+      productId: el.id,
+      quantity: el.quantity,
+      saleId: idSale }));
+
+    await SalesProducts.bulkCreate(editedCart, { transaction: trans });
+
+    return saleObj;
   });
-  return saleObj;
+  return resultCreateSale;
 };
 
 const findOne = async ({ id }) => {
-  const userObj = await sales.findOne({ where: { id } });
+  const userObj = await Sales.findOne({ where: { id } });
   return userObj;
 };
 
-const update = async (id, userRole) => sales.findOne(id, userRole);
+const update = async (id, userRole) => Sales.findOne(id, userRole);
 
 module.exports = {
   findAll,
--- back-end/src/repositories/register.repository.js
+++ back-end/src/repositories/register.repository.js
@@ -1,13 +1,13 @@
 const { Op } = require('sequelize');
-const { users } = require('../database/models');
+const { Users } = require('../database/models');
 
 const getUser = async ({ name, email }) => {
-  const userObj = await users.findOne({ where: { [Op.or]: [{ name }, { email }] } });
+  const userObj = await Users.findOne({ where: { [Op.or]: [{ name }, { email }] } });
   return userObj;
 };
 
 const postUser = async ({ name, email, password, role }) => {
-  const newUser = await users.create({ name, email, password, role });
+  const newUser = await Users.create({ name, email, password, role });
   return newUser;
 };
 
