4eae4b89a9fb0be8fafbf178b83213fa10b264f2
ottomicheletti <michelettiotto@gmail.com>
2022-10-04 04:45:52 -0300

    refactor: :recycle: Changes the HTTP verb on the endpoint /customer/orders

--- back-end/src/routes/orders.routes.js
+++ back-end/src/routes/orders.routes.js
@@ -2,8 +2,8 @@ const express = require('express');
 const salesController = require('../controllers/sales.controller');
 const tokenValidator = require('../middlewares/token.validator');
 
-const router = express.Router({ mergeParams: true });
+const router = express.Router();
 
-router.get('/:id', tokenValidator, (req, res) => salesController.getOrdersByClientId(req, res));
+router.post('/', tokenValidator, (req, res) => salesController.getOrdersByUserId(req, res));
 
 module.exports = router;
