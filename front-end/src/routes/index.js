3da3459bf20f9557c26a138ca3ebede677b92c0d
ottomicheletti <michelettiotto@gmail.com>
2022-10-04 04:49:46 -0300

    feat: :sparkles: Creates new routes for /customers/orders and for orders details on /customers/orders/:orderId

--- front-end/src/routes/index.js
+++ front-end/src/routes/index.js
@@ -4,6 +4,8 @@ import CustomerProducts from '../pages/customerProducts';
 import Login from '../pages/login';
 import Register from '../pages/register';
 import CustomerCheckout from '../pages/customerCheckout';
+import CustomerOrderDetails from '../pages/customerOrderDetails';
+import CustomerOrders from '../pages/customerOrders';
 
 function IndexRouter() {
   return (
@@ -13,16 +15,11 @@ function IndexRouter() {
       <Route path="/register" element={ <Register /> } />
       <Route path="/customer/products" element={ <CustomerProducts /> } />
       <Route path="/customer/checkout" element={ <CustomerCheckout /> } />
+      <Route path="/customer/orders" element={ <CustomerOrders /> } />
+      <Route path="/customer/orders/:orderId" element={ <CustomerOrderDetails /> } />
+      {/* <Route path="/customer/orders/" element={ <CustomerOrders /> } /> */}
       {/* <Route path="/produtos" element={<Catalogo />} />
       <Route path="/catalogo/:id" element={<CatalogoId />} />
-      <Route path="/store" element={<Store />} />
-      <Route path="/perfil" element={<Perfil />} />
-      <Route path="/clube" element={<Clube />} />
-      <Route path="/loja" element={<Loja />} />
-      <Route path="/app" element={<AppWine />} />
-      <Route path="/eventos" element={<Eventos />} />
-      <Route path="/ofertas" element={<Ofertas />} />
-      <Route path="/planos/:id" element={<PlanosId />} />
       <Route path="*" element={<NotFound />} /> */}
     </Routes>
   );
